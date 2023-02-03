6cdb8ffa5b38f835667fcc9fcd2e36fa29e6a41f
Marcio Dias <67355232+MarcioDias83@users.noreply.github.com>
2023-02-03 04:00:54 -0300

    Delete Python2 directory

--- Python2/PalavrasAninhadas3.py
+++
@@ -1,4 +0,0 @@
-# Escreva um programa para gerenciar uma lista aninhada de tarefas. O programa deve permitir que o usuário adicione novas tarefas e
-# exiba a lista final. Também deve permitir que o usuário remova tarefas existentes.
-
-lista = []
