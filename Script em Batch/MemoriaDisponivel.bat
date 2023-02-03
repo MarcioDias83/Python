1a83e1d2bce7c69e325efb7c286c4ad036cf8f4d
Marcio Dias <67355232+MarcioDias83@users.noreply.github.com>
2023-02-03 19:35:11 -0300

    Delete Script em Batch directory

--- Script em Batch/MemoriaDisponivel.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:loop
-for /F "tokens=2 delims==" %%a in ('wmic OS get FreePhysicalMemory /Value ^| find "="') do set _mem=%%a
-set /a _mem=%_mem%/1024
-echo Memoria RAM Disponivel: %_mem% MB
-ping localhost -n 2 >nul
-goto loop
